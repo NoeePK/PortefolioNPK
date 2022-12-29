875b37b07e2e8f9b6c5593866ccf44010a449e8b
Noee Pisano <noeepisanokoffel@gmail.com>
2022-12-29 14:51:19 +0100

    initialisation du projet

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+// Style applicable à toute l'application
+import "./styles/compiled/index.css";
+// Ajouter les mediaqueries ici
+
+const root_container = document.getElementById("root");
+const root = createRoot(root_container);
+
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
