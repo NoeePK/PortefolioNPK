8d23b6d8f43dc0eb74a2f614ce4b8f19388e8ac9
Noee Pisano <noeepisanokoffel@gmail.com>
2023-01-22 12:33:31 +0100

    style logo v1 OK

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ import github from "../assets/github.svg";
 const Footer = () => {
     return (
         <footer>
-            <Link to="">
+            <Link to="" className="logo-style">
                 <img src={linkedin} alt="Page LinkedIn" />
                 <h3>linkedIn</h3>
             </Link>
-            <Link to="">
+            <Link to="" className="logo-style">
                 <img src={github} alt="Page GitHub" />
                 <h3>github</h3>
             </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,34 +18,32 @@ const Home = () => {
                     Manifeste sur ma vision du code/développement, ce que j'aime
                     et recherche
                 </p>
-                <div className="tools-wrap">
-                    <ul className="tools-list">
-                        <li>
-                            <img src={javascript} alt="Javascript" />
-                            <h3>Javascript</h3>
-                        </li>
-                        <li>
-                            <img src={react} alt="React" />
-                            <h3>React</h3>
-                        </li>
-                        <li>
-                            <img src={nodejs} alt="Node-js" />
-                            <h3>Node.js</h3>
-                        </li>
-                        <li>
-                            <img src={sass} alt="Sass" />
-                            <h3>Sass</h3>
-                        </li>
-                        <li>
-                            <img src={html} alt="HTML" />
-                            <h3>HTML</h3>
-                        </li>
-                        <li>
-                            <img src={css} alt="CSS" />
-                            <h3>CSS</h3>
-                        </li>
-                    </ul>
-                </div>
+                <ul className="tools-list">
+                    <li className="logo-style">
+                        <img src={javascript} alt="Javascript" />
+                        <h3>Javascript</h3>
+                    </li>
+                    <li className="logo-style">
+                        <img src={react} alt="React" />
+                        <h3>React</h3>
+                    </li>
+                    <li className="logo-style">
+                        <img src={nodejs} alt="Node-js" />
+                        <h3>Node.js</h3>
+                    </li>
+                    <li className="logo-style">
+                        <img src={sass} alt="Sass" />
+                        <h3>Sass</h3>
+                    </li>
+                    <li className="logo-style">
+                        <img src={html} alt="HTML" />
+                        <h3>HTML</h3>
+                    </li>
+                    <li className="logo-style">
+                        <img src={css} alt="CSS" />
+                        <h3>CSS</h3>
+                    </li>
+                </ul>
             </article>
             <div className="thumbs-wrap">
                 <Thumb />
